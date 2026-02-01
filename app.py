fe3fd3a00e1cc6ce6d045bbe75e4be8fb8ee0ecc
Sanjana-tech16 <sanjanasan1606@gmail.com>
2026-02-01 11:51:19 +0530

    Database: Create Task model with relationship to User

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = "secret_key"
 
-# Database Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -16,6 +15,17 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    complete = db.Column(db.Boolean, default=False)
+    # Foreign Key to link task to a user
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Task {self.id}>'
+    
 @app.route('/')
 def home():
     return "<h1>Project is running!</h1>"
