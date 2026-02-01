3a6491adfdef5e5e078fac890ef41072ad6ce097
Sanjana-tech16 <sanjanasan1606@gmail.com>
2026-02-01 11:47:51 +0530

    Backend: Configure SQLite database connection

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "secret_key"
+
+# Database Configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
